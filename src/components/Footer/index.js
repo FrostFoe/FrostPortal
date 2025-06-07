ce56c64f6144bea6adc4c10475907ea2b64cb0a5
frostfoe <frostfoe@gmail.com>
2025-06-07 06:06:30 +0000

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -81,7 +81,7 @@ const Footer = () => {
 
       <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
         <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
+          &copy;2025 FrostFoe. All rights reserved.
         </span>
         <Link
           href="/sitemap.xml"
@@ -96,7 +96,7 @@ const Footer = () => {
             className="underline"
             target="_blank"
           >
-            CodeBucks
+            FrostFoe
           </a>
         </div>
       </div>
